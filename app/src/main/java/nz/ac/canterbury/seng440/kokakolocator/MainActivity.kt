0e1319ac51e1fad5462a1dbafe9e0b85b64d660b
Jack Steel <jes143@uclive.ac.nz>
2019-05-06 14:14:07 +1200

    Initial empty project creation

---
+++ app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/MainActivity.kt
@@ -0,0 +1,12 @@
+package nz.ac.canterbury.seng440.kokakolocator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
