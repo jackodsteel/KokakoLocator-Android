5700a9a590b1d78c335407748c6c844cc1b24a59
Tom Kearsley <tke29@uclive.ac.nz>
2019-05-09 15:35:32 +1200

    - Created intial login/signup splash screen - Created Recordings Recycler page - Created Recordings Adapter.

---
+++ app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/RecordingsAdapter.kt
@@ -0,0 +1,29 @@
+package nz.ac.canterbury.seng440.kokakolocator
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class RecordingsAdapter(private val Dataset: Array<String>):
+    RecyclerView.Adapter<RecordingsAdapter.ViewHolder>(){
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recordings,parent,false) as TextView
+
+        return ViewHolder(textView)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        return Dataset.size
+    }
+
+}
