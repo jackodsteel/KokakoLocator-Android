8b0715c587f694c7f128df156e64b1c462258490
Thomas Kearsley <tke29@uclive.ac.nz>
2019-05-10 21:33:35 +1200

    Added GPS Tracking

--- app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/MainActivity.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/MainActivity.kt
@@ -1,12 +1,80 @@
 package nz.ac.canterbury.seng440.kokakolocator
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    var fusedLocationClient: FusedLocationProviderClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.
+            getFusedLocationProviderClient(this)
+        getLocation()
     }
+
+    fun getLocation(){
+        if (checkPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            fusedLocationClient?.lastLocation?.
+                addOnSuccessListener(this,
+                    {location : Location? ->
+                        // Got last known location. In some rare
+                        // situations this can be null.
+                        if(location == null) {
+                            // TODO, handle it
+                        } else location.apply {
+                            // Handle location object
+                            println(location.toString())
+                            Log.e("LOG", location.toString())
+                        }
+                    })
+        }
+
+    }
+
+    val PERMISSION_ID = 42
+    private fun checkPermission(vararg perm:String) : Boolean {
+        val havePermissions = perm.toList().all {
+            ContextCompat.checkSelfPermission(this,it) ==
+                    PackageManager.PERMISSION_GRANTED
+        }
+        if (!havePermissions) {
+            if(perm.toList().any {
+                    ActivityCompat.
+                        shouldShowRequestPermissionRationale(this, it)}
+            ) {
+                val dialog = AlertDialog.Builder(this)
+                    .setTitle("Permission")
+                    .setMessage("Permission needed!")
+                    .setPositiveButton("OK", {id, v ->
+                        ActivityCompat.requestPermissions(
+                            this, perm, PERMISSION_ID)
+                    })
+                    .setNegativeButton("No", {id, v -> })
+                    .create()
+                dialog.show()
+            } else {
+                ActivityCompat.requestPermissions(this, perm, PERMISSION_ID)
+            }
+            return false
+        }
+        return true
+    }
+
+
+
+
 }
--- app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/Recordings.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/Recordings.kt
@@ -3,6 +3,7 @@ package nz.ac.canterbury.seng440.kokakolocator
 import android.app.Activity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.recyclerview.widget.RecyclerView
 
 class Recordings : Activity() {
@@ -10,19 +11,26 @@ class Recordings : Activity() {
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recordings)
 
-        var dataSet = arrayOf("Test Co-ords")
-
-
-
+        var recordingsList = arrayOf("Test")
 
         viewManager = LinearLayoutManager(this)
-        viewAdapter = RecordingsAdapter(dataSet)
+        viewAdapter = RecordingsAdapter(recordingsList)
 
-        //recyclerView = findViewById<RecyclerView>(R.id.recor)
+        recyclerView = findViewById<RecyclerView>(R.id.coords).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
 
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            adapter = viewAdapter
+
+        }
     }
 }
