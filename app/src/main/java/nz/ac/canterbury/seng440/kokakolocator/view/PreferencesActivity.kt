4a03105181e904c2f7858407056cf92c7b2cb64e
Jack Steel <jes143@uclive.ac.nz>
2019-05-27 22:24:50 +1200

    Add prefs page

--- app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/view/PreferencesActivity.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/view/PreferencesActivity.kt
@@ -2,6 +2,7 @@ package nz.ac.canterbury.seng440.kokakolocator.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
 import nz.ac.canterbury.seng440.kokakolocator.R
 
 class PreferencesActivity : AppCompatActivity() {
@@ -9,5 +10,19 @@ class PreferencesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(
+                R.id.preferences,
+                PreferencesFragment()
+            )
+            .commit()
     }
+
+    class PreferencesFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+
 }
