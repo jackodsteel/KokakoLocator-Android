7aa0594f927b00e038ed676357f73a91c05d2b78
Jack Steel <jes143@uclive.ac.nz>
2019-05-31 13:11:43 +1200

    Bump versions for release.

--- app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/database/RecordingDatabase.kt
+++ app/src/main/java/nz/ac/canterbury/seng440/kokakolocator/database/RecordingDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Recording::class], version = 6, exportSchema = false)
+@Database(entities = [Recording::class], version = 7, exportSchema = false)
 @TypeConverters(DateConverter::class, LatLngConverter::class)
 abstract class RecordingDatabase : RoomDatabase() {
     abstract fun recordingDao(): RecordingDao
